74e9393a979d5fdfc48b0c08acea772cf393918c
Eraea <44805352+Eraea@users.noreply.github.com>
2023-09-20 00:43:58 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.riotgames.league.wildrift"
+#define GamePackageName "com.square_enix.android_googleplay.ff7ecww"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
